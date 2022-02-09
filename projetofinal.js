e0db383efee69d67a92be88c73d2b6e8c208dcb0
Wellington Oliveira <jwell.oliveira@gmail.com>
2022-02-08 22:36:50 -0300

    Atualização do codigo, ja com uma estrutura de repetição e objeto para guarda informaçoes do jogador

--- projetofinal.js
+++ projetofinal.js
@@ -1,25 +1,52 @@
 var prompt = require('prompt-sync')();
 
 // Inicio do Jogo
-let cont = 0;
+var cont = 0;
 const jogador = {
     nome: 'Gale',
-    local: 'Casa',
     statusJogador: '', 
 };
 
-console.log('Após um longo dia, Gale chega em casa, em meio uma tempestade. Como de costume vai tomar seu banho, ao terminar o banho acaba a energia, então ele...\n');
-console.log('Desejar ir Dormir ou Acender Uma lanterna ?\n ')
-jogador.statusJogador = prompt('Digite entre opções Dormir Ou laterna: ').toUpperCase();
+console.log('\n Após um longo dia, Gale chega em casa, em meio uma tempestade. Como de costume vai tomar seu banho, ao terminar o banho acaba a energia, então ele...\n');
+console.log(' Desejar ir Dormir ou Acender Uma lanterna ?\n ')
+jogador.statusJogador = prompt(' Digite entre opções Dormir Ou laterna: ').toUpperCase();
+
+while (jogador.statusJogador != 'DORMIR' && jogador.statusJogador != 'LANTERNA') {
+    
+    console.log('\n Errada, escolha entre opçoes abaixo:')
+    
+    jogador.statusJogador = prompt(' Digite entre opções Dormir Ou laterna: ').toUpperCase();
+}
 
 while (jogador.statusJogador === 'DORMIR') {
-    console.log('\nGale resolve então deitar para descansar, pois seus dias costumam ser longos e ele tem uma rotina a cumprir.');
 
-    cont++;
+    console.clear();// Função para limpar terminal. 
+
+    console.log('\n Gale resolve então deitar para descansar, pois seus dias costumam ser longos e ele tem uma rotina a cumprir.');
+    
+    break;
 }
+
 while (jogador.statusJogador === 'LANTERNA') {
+    
+    console.clear();// Função para limpar terminal.
+
     console.log('\n Ao acender a lanterna: A lanterna é mágica e levou ele há um Universo Paralelo, no qual existe uma missão a ser cumprida, o que ele deve fazer? ');
+    
+    console.log('\n Desejar realizar a (S) Missao ou (N) Desligar a lanterna?');
+    
+    jogador.statusJogador = prompt('\n Escolha entre Opçao S ou N : ').toUpperCase();
 
-    cont++;
+    while (jogador.statusJogador === 'S') {
+
+        console.clear();// Função para limpar terminal. 
+        
+    }
+
+    while (jogador.statusJogador === 'N') {
+
+        console.clear();// Função para limpar terminal. 
+        
+    }
+
 }
-
